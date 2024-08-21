0910a2a4437c2516259d749b89a09103e8d2d845
AUGUSTO SÉRGIO DOS SANTOS COSTA <augusto.costa@GRANBERY.local>
2024-08-21 20:36:31 -0300

    Alterar Teste

--- singleton-exercise/src/test/java/ValidadorNotificacaoTest.java
+++ singleton-exercise/src/test/java/ValidadorNotificacaoTest.java
@@ -5,14 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class ValidadorNotificacaoTest {
 
     @Test
-    public void deveRetornarNomeValidador(){
+    public void deveRetornarNomeValidador() {
         ValidadorNotificacao.getInstance().setNomeValidador("Horário de Ponto");
 
         assertEquals("Horário de Ponto", ValidadorNotificacao.getInstance().getNomeValidador());
     }
 
     @Test
-    public void deveRetornar(){
+    public void deveRetornarEnvioNotificacao() {
         ValidadorNotificacao.getInstance().setEnvioNotificacao("Ponto Registrado");
 
         assertEquals("Ponto Registrado", ValidadorNotificacao.getInstance().getEnvioNotificacao());
